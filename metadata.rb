e98a06e9dba5adff704862d714e14aee7581331c
Sauraus <antek.baranski@gmail.com>
2014-10-16 17:33:37 -0700

    Fix version number

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "antek.baranski@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures perforce"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.0"
+version          "2.0.0"
 supports         "mac_os_x"
 name             "perforce"
